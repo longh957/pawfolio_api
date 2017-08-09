01450b1383f676e7737cd6659c50124a105af117
Long Huynh <longh957@gmail.com>
2017-08-08 20:52:38 -0700

    add ruby versions and procfile

---
+++ Procfile
@@ -0,0 +1,4 @@
+web: bundle exec rails s Puma
+/* resque_scheduler: env TERM_CHILD=1 bundle exec rake resque:scheduler */
+/* resque: env TERM_CHILD=1 RESQUE_TERM_TIMEOUT=7 bundle exec rake resque:work */
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: redirect('http://www.longkhuynh.com')
   namespace :api do
     namespace :v1 do
       resource :authentication, only: %i[create]
