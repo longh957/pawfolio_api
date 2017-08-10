3f231ad9fda338801d55a737c286a9e30fb383de
longh957 <longh957@gmail.com>
2017-08-09 21:21:34 -0700

    creates user registration controller (#2)

---
+++ app/controllers/api/v1/registrations_controller.rb
@@ -0,0 +1,43 @@
+module Api
+  module V1
+    class RegistrationsController < ApplicationController
+      skip_before_action :authenticate_user, only: [:create]
+
+      def create
+        user = User.create(create_params)
+        if user.persisted?
+          render json: { message: 'Success' }, status: :created
+        else
+          render json: { error: user.errors.full_messages }, status: :bad_request
+        end
+      end
+
+      def update
+        if @current_user.update(update_params)
+          render json: { message: 'Success' }, status: :accepted
+        else
+          render json: { error: @current_user.errors.full_messages }, stautus: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @current_user.delete
+        if @current_user.persisted?
+          render json: { error: 'Bad Request' }, status: :bad_request
+        else
+          render json: { message: 'Success' }, status: :accepted
+        end
+      end
+
+      private
+
+      def create_params
+        params.permit(:name, :email, :password)
+      end
+
+      def update_params
+        params.permit(:name, :email)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resource :authentication, only: %i[create]
+      resource :registration, only: %i[create update destroy]
     end
   end
 end
