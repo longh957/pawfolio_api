6b6ecd367550e89e2e96851ac8328464ac063546
Long Huynh <longh957@gmail.com>
2017-08-15 17:36:58 -0700

    remove cors rule

--- config/application.rb
+++ config/application.rb
@@ -34,12 +34,6 @@ module PawfolioBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :put, :delete]
-      end
-    end
     config.eager_load_paths += Dir[Rails.root.join('lib')]
     config.eager_load_paths += Dir[Rails.root.join('lib', 'exceptions')]
   end
